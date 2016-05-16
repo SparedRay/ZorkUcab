c23813303399084f0ed2e1e395a473e088eae4c4
Alfredo Cabello <>
2016-05-16 11:24:16 -0430

    Proyecto iniciado

---
+++ src/zorkucab/ZorkUcab.java
@@ -0,0 +1,16 @@
+package zorkucab;
+
+/**
+ *
+    * @author SparedRay
+ */
+public class ZorkUcab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
