c3989513cca50df10a5587b4ec23df508db0071c
Martinor <icehealt.alf@gmail.com>
2016-05-18 19:59:58 -0400

    Cambio a interfaz para reutilizacion logica

--- src/zengine/Juego.java
+++ src/zengine/Juego.java
@@ -4,22 +4,8 @@ package zengine;
  *
  * @author SparedRay
  */
-public class Juego {
-    private Jugador jugador;
-    
-    public Juego() {
-        this.jugador = new Jugador();
-    }
-    
-    public void iniciar() {
-        System.out.println("Partida iniciada");
-    }
-    
-    public void run() {
-        System.out.println("Partida en curso");
-    }
-    
-    public boolean continuar() {
-        return jugador.vivo();
-    }
+public interface Juego {
+    public void iniciar();
+    public void run();
+    public boolean continuar();
 }
