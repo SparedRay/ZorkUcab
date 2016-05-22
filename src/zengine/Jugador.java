8c897381ba9f0c26b29bd85a3b59ae8a05816ba1
Martinor <icehealt.alf@gmail.com>
2016-05-22 17:49:36 -0400

    Agregado atributo de nombre, para personalizacion de partida

--- src/zengine/Jugador.java
+++ src/zengine/Jugador.java
@@ -6,12 +6,23 @@ package zengine;
  */
 public class Jugador extends Entidad {
 
-    public Jugador(float hp, float ataque, float defensa) {
+    private String nombre;
+
+    public Jugador(String nombre, float hp, float ataque, float defensa) {
         super(hp, ataque, defensa);
+        this.nombre = nombre;
+    }
+
+    public Jugador(float hp, float ataque, float defensa) {
+        this("", hp, ataque, defensa);
+    }
+
+    public Jugador(String nombre) {
+        this(nombre, 100, 10, 0);
     }
     
     public Jugador() {
-        this(100, 10, 0);
+        this("", 100, 10, 0);
     }
     
     public boolean vivo() {
@@ -20,7 +31,7 @@ public class Jugador extends Entidad {
     
     @Override
     public void mostrar() {
-        System.out.println("STATS:");
+        System.out.println(nombre + " STATS:");
         System.out.println("hp: " + this.hp);
         System.out.println("Ataque: " + this.ataque);
         System.out.println("Defensa: " + this.defensa);
